5e1d3fea4728ba809d2c424e03d9570b50327412
Atchuth02 <167709287+Atchuth02@users.noreply.github.com>
2025-07-09 16:32:17 +0530

    Update urls.py

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 from .views import create_ticket
 from .views import user_dashboard
+from .views import admin_dashboard
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('create-ticket/', create_ticket, name='create_ticket'),
      path('dashboard/', user_dashboard, name='ticket_list'),
+     path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
 ]
